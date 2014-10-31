679b9ba622639beae2ddb2ac993b61c038bc17cc
Shayne Snow <ssnow@mitre.org>
2014-10-31 12:35:31 -0400

    Support successfully creating new profiles

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -17,4 +17,17 @@ class ProfileController < ApplicationController
 
     def edit
     end
+
+    def upload
+        uploaded = JSON.parse(request.POST['profile'])
+        @@new_file = uploaded.to_yaml
+        head :created
+    end
+
+    def download
+        send_data(@@new_file, type: 'application/x-yaml', disposition: 'attachment', filename: 'newprofile.yaml')
+    end
+
+    def created
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
     get "/profile/create", to: "profile#create"
     get "/profile/edit", to: "profile#edit"
     get "/profile/compare", to: "profile#compare"
+
+    post "/profile/upload", to: "profile#upload"
+    get "/profile/created", to: "profile#created"
+    get "/profile/download", to: "profile#download"
 end
